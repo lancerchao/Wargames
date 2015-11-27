7377966600cfeae38b5cae415b6ae4228c21a8c6
root <root@localhost.localdomain>
2015-11-27 06:52:55 -0500

    Maze2 in progress

--- OverTheWire/Maze/Maze1/test.c
+++ OverTheWire/Maze/Maze1/test.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-void puts(char *s)
+#include <unistd.h>
+#include <sys/types.h>
+#define _GNU_SOURCE
+int puts(const char *str)
 {
-	printf("Try me");
+    int ret = setresuid(geteuid(), geteuid(), geteuid());
+    printf("Setresuid returned %d\n", ret);
+    FILE *f = fopen("/etc/maze_pass/maze2", "r");
+    char buf[100];
+    fscanf(f, "%s", buf);
+    printf("Password: %s\n", buf);
+    return 0;
 }
